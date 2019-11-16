e23b69d2e293027d95b9fa346af5d697186bc603
Ramon Perez <ramonprz01@gmail.com>
2019-11-16 23:53:53 +1100

    Day 15: full first page dataset

---
+++ aaspider.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class AaspiderSpider(scrapy.Spider):
+    name = 'aaspider'
+    allowed_domains = ['ifar.org']
+    start_urls = ['http://www.ifar.org/catalogues_raisonnes.php?alpha=&searchtype=artist&published=1&inPrep=1&artist=&author=']
+
+    def parse(self, response):
+        self.log('I just scraped: ' + response.url)
+        yield {
+            'name': response.css('td.g_center_name > a::text').extract(),
+            'link': response.css('td.g_center_name > a::attr(href)').extract(),
+            'birth_year': response.css('.g_center_birth_year').extract(),
+            'birth_place': response.css('.g_center_birth_place').extract(),
+            'death_year': response.css('.g_center_death_year').extract(),
+            'death_place': response.css('.g_center_death_place').extract()
+        }
+        
+        
+        
