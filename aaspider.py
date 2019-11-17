bc31dbe8b6f0093f460385faf9e9111eacb381e2
Ramon Perez <ramonprz01@gmail.com>
2019-11-17 15:31:33 +1100

    Day 17: getting closer

--- aaspider.py
+++ aaspider.py
@@ -9,19 +9,37 @@ class AaspiderSpider(scrapy.Spider):
     prefix = "http://www.ifar.org/"
     
 
+    def start_requests(self):
+        yield scrapy.Request(url = start_urls, callback = self.parse)
+    
     def parse(self, response):
         self.log('I just scraped: ' + response.url)
-        yield {
-            'name': response.css('td.g_center_name > a::text').extract(),
-            'link': response.css('td.g_center_name > a::attr(href)').extract(),
-            'birth_year': response.css('.g_center_birth_year').extract(),
-            'birth_place': response.css('.g_center_birth_place').extract(),
-            'death_year': response.css('.g_center_death_year').extract(),
-            'death_place': response.css('.g_center_death_place').extract()
-        }
         
+        name = response.css('td.g_center_name > a::text').extract(),
+        link = response.css('td.g_center_name > a::attr(href)').extract(),
+        birth_year =  response.css('.g_center_birth_year').extract(),
+        birth_place = response.css('.g_center_birth_place').extract(),
+        death_year = response.css('.g_center_death_year').extract(),
+        death_place = response.css('.g_center_death_place').extract()
         
         urls = response.css('td.g_center_name > a::attr(href)').extract()
         for url in urls:
             url = prefix.join(url)
-            yield scrapy.Request(url=url, callback)
+            yield scrapy.Request(url=url, callback = self.parse_details)
+            
+    
+    def parse_details(self, response):
+        box_info = response.css('div#box_2_left ::text').extract()
+        catalogues_links = response.css('.catalogue_title > a::attr(href)').extract()
+        
+        urls_2 = response.css('.catalogue_title > a::attr(href)').extract()
+        for url in urls_2:
+            url = prefix.join(url)
+            yield scrapy.Request(url=url, callback = self.parse_details_dos)
+            
+    
+    def parse_details_dos(self, response):
+        yield {
+            'column_names' : response.css('.book_title > dl > dt::text').extract(),
+            'content' : response.css('.book_title > dl > dd::text').extract()
+        }
