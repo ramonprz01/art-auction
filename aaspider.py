2fac9115f530ef007f3a283201117fed140f176f
Ramon Perez <ramonprz01@gmail.com>
2019-11-25 12:10:36 +0930

    Full Dataset in JSON format

--- aaspider.py
+++ aaspider.py
@@ -11,6 +11,16 @@ class AaspiderSpider(scrapy.Spider):
     
     
     def parse(self, response):
+        
+        yield {
+            'name': response.css('td.g_center_name > a::text').extract(),
+            'link': response.css('td.g_center_name > a::attr(href)').extract(),
+            'birth_year': response.css('.g_center_birth_year').extract(),
+            'birth_place': response.css('.g_center_birth_place').extract(),
+            'death_year': response.css('.g_center_death_year').extract(),
+            'death_place': response.css('.g_center_death_place').extract()
+        }
+        
         links_info = response.css('td.g_center_name > a::attr(href)')
         links = links_info.extract()
         href = []
@@ -20,18 +30,15 @@ class AaspiderSpider(scrapy.Spider):
         for i in href:
             yield scrapy.Request(i, callback=self.parse_details) 
             
-#            yield response.follow(url = href, callback = self.parse_details)
-
-#        for url in response.css('.catalogue_title > a::attr(href)').extract():
-#            url_new = prefix.join(url)
-#            yield response.follow(url_new, callback = self.parse_details_dos)
-            
     
     def parse_details(self, response):
         
         yield {
+            'artist_one' : response.css('h1::text').extract_first(),
             'box_info' : response.css('div#box_2_left ::text').extract()
             }
+        
+        
         new_info = response.css('.catalogue_title > a::attr(href)')
         new_links = new_info.extract()
         href_new = []
@@ -41,19 +48,11 @@ class AaspiderSpider(scrapy.Spider):
         for s in href_new:
             yield scrapy.Request(s, callback=self.parse_details_dos)
         
-        
-#        yield {
-#            'name': response.css('td.g_center_name > a::text').extract(),
-#            'link': response.css('td.g_center_name > a::attr(href)').extract(),
-#            'birth_year': response.css('.g_center_birth_year').extract(),
-#            'birth_place': response.css('.g_center_birth_place').extract(),
-#            'death_year': response.css('.g_center_death_year').extract(),
-#            'death_place': response.css('.g_center_death_place').extract()
-#        }
      
     def parse_details_dos(self, response):
 
         yield {
+            'artist_two' : response.css('h1::text').extract_first(),
             'column_names' : response.css('.book_title > dl > dt::text').extract(),
             'content' : response.css('.book_title > dl > dd::text').extract()
         }
